0937c0cb65afd72172381248d3a09be4dd3aeb0b
lingeswaran <lingeswaran@qadex.com>
2022-12-14 22:08:30 +0530

    basic structure implemented

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
+  audited
+  has_person_name
+  has_address :address
+
+  before_create :generate_token
+
+  protected
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless User.exists?(token: random_token)
+    end
+  end
+end
